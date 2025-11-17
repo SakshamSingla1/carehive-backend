7fa0cdcb4e213bf5944767cb54c59bffe694b20a
Saksham Singla <90709664+SakshamSingla1@users.noreply.github.com>
2025-11-17 11:34:00 +0530

    Added the frontend URL

--- src/main/java/com/careHive/serviceImpl/PaymentServiceImpl.java
+++ src/main/java/com/careHive/serviceImpl/PaymentServiceImpl.java
@@ -17,7 +17,6 @@ import com.careHive.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
