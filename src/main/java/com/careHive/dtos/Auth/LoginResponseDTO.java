b5c1496d51d3cc3f345c4a7c98d566668f153661
Saksham Singla <90709664+SakshamSingla1@users.noreply.github.com>
2025-11-29 01:25:20 +0530

    COLOR THEME set by Login Role (#17)

--- src/main/java/com/careHive/dtos/Auth/LoginResponseDTO.java
+++ src/main/java/com/careHive/dtos/Auth/LoginResponseDTO.java
@@ -1,13 +1,23 @@
 package com.careHive.dtos.Auth;
 
+import com.careHive.dtos.ColorTheme.ColorThemeResponseDTO;
+import com.careHive.entities.ColorTheme;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Builder
 public class LoginResponseDTO {
-    private String token;
+    private String id;
+    private String name;
+    private String username;
+    private String phone;
     private String email;
     private String role;
-    private String message;
+    private String token;
+
+    private List<ColorTheme> themes;       // ← all themes for this role
+    private ColorTheme defaultTheme;        // ← default theme for usage
 }
